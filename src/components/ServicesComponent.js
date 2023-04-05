8b8d6d6243f06d6a0214d66d67e8a98c89d30e14
BendLac <bendelaszloadam@gmail.com>
2023-04-05 13:00:08 +0200

    services basics done without animation

--- src/components/ServicesComponent.js
+++ src/components/ServicesComponent.js
@@ -12,7 +12,11 @@ function Services() {
             </div>
           </div>
           <div className='services-dynamic'>
-          
+            <div className=''>
+              <h1 className='black service-dynamic-header'>Design</h1>
+              <p className='medium service-dynamic-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <button className='button-rounded arrow-button white-arrow-button'></button>  
+            </div>
           </div>
         </div>
       </div>
